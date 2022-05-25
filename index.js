26c0c067515b8e848650c0d60dbdcf706b8bda8d
Your full name <dariodelrio14@gmail.com>
2022-05-24 20:57:09 -0400

    2

--- index.js
+++ index.js
@@ -1,39 +1,90 @@
-// Objects
-let student1 = {
-  firstName: 'Lex',
-  lastName: 'Perez',
-  State: 'NY',
-  age: 23,
-}
+// // Objects
+// let student1 = {
+//   firstName: 'Lex',
+//   lastName: 'Perez',
+//   State: 'NY',
+//   age: 23,
+// }
 
-let student2 = {
-  firstName: 'John',
-  lastName: 'War',
-  State: 'LA',
-  age: 25,
-}
+// let student2 = {
+//   firstName: 'John',
+//   lastName: 'War',
+//   State: 'LA',
+//   age: 25,
+// }
 
-let student3 = {
+// let student3 = {
+//   firstName: 'Dario',
+//   lastName: 'del Rio',
+//   State: 'FL',
+//   age: 27,
+// }
+
+// console.log()
+
+// let selectorColors = ['red', 'blue', 'green'] // Arrays
+// selectorColors.push = 'brown'
+// console.log(selectorColors.length)
+
+// // Funtion performing a task
+// function greet(name, age) {
+//   console.log('Hello ' + name, 'you are ' + age, 'years old')
+// }
+// greet('Dario', 27)
+
+// // Function calculating a value
+// function square(number) {
+//   return number * number
+// }
+// console.log(square(2))
+
+// Functions
+
+// function Car(brand, model) {
+//   console.log('The car is a ' + brand + ' and the model is ' + model)
+// }
+// Car('Honda', 'Accord')
+// Same as an Arrow Function
+
+let car = (br, mod) => console.log('The car is a ' + br + ' ' + mod)
+car('Honda', 'Accord')
+
+let Flower = (type, color) =>
+  console.log('The flower is a ' + type + ' ' + 'and it is ' + color)
+Flower('Margarita', 'White')
+
+let sum = (a, b) => console.log(a + b)
+sum(1, 2)
+
+function suma(e, f) {
+  console.log(e + f)
+}
+suma(1, 2)
+
+// More Objects
+const Student = {
   firstName: 'Dario',
   lastName: 'del Rio',
-  State: 'FL',
   age: 27,
+  eyeColor: 'brown',
 }
 
-console.log()
+// More Objects
+let person = new Object()
 
-let selectorColors = ['red', 'blue', 'green'] // Arrays
-selectorColors.push = 'brown'
-console.log(selectorColors.length)
-
-// Funtion performing a task
-function greet(name, age) {
-  console.log('Hello ' + name, 'you are ' + age, 'years old')
+person.name = 'Dario'
+person.eyeColor = 'brown'
+person.age = 27
+person.updateAge = function () {
+  return ++person.age
 }
-greet('Dario', 27)
+console.log(person)
 
-// Function calculating a value
-function square(number) {
-  return number * number
+let alum1 = {
+  name: 'Daniel',
+  eyeColor: 'blue',
+  age: 27,
+  updateAge: function () {
+    return ++alum1.name
+  },
 }
-console.log(square(2))
