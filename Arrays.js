790a135394924d9ae3ca023980a1b32319cfa1f1
Your full name <dariodelrio14@gmail.com>
2022-05-30 17:07:27 -0400

    Arrays2

--- Arrays.js
+++ Arrays.js
@@ -10,8 +10,12 @@ console.log(num1.split())
 console.log(num2.split())
 console.log(num3.split())
 
-const fruits = ['Banana', 'Orange', 'Apple', 'Mango']
-console.log(fruits.length)
+const fruits = ['Banana', 'Orange', 'Apple', 'Mango'] // Check if at least one item is in the array
+const result = fruits.some(itemInArray)
+
+function itemInArray(itemF) {
+  return itemF == 'Banana'
+}
 
 const n = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 console.log(n[4])
@@ -41,7 +45,26 @@ const carBrands = [
   'Volvo',
   'Wolkswagen',
 ]
-carBrands.length(checkStartH) // Check the car brands that start with letter "H"
-function checkStartH(H) {
-  return H === 'H'
+const res = carBrands.some(letterH) // Function to check if at least one item is in the array
+
+function letterH(letter) {
+  return letter == 'Ferrari'
 }
+console.log(res)
+console.log(
+  '-----------------------------------------------------------------------------------'
+)
+//----------------------------------------------------------------------------------------------
+
+const alumns = ['John', 'Max', 'Lucas', 'Antonio', 'Edgar'] // pop, shift, spice, filter
+alumns.splice(3, 1)
+console.log(alumns)
+
+// Other examples of the use of "some"
+
+var ages = [12, 15, 2, 7, 19, 35, 57, 14, 21, 29, 26, 15, 22]
+ages.some(checkAdult)
+function checkAdult(age) {
+  return age > 18
+}
+console.log(ages)
